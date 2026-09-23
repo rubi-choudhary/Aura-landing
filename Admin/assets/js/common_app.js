6571615aee19844cf513f1674c56ca2274517e31
Rubi Choudhary <rubi@nextwebi.co.in>
2026-09-23 16:18:53 +0530

    Add Admin/assets/js/common_app.js

---
+++ Admin/assets/js/common_app.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $('.nospecial').keypress(function (e) {
+        var charCode = (e.which) ? e.which : event.keyCode
+        if (charCode != 32)
+            if (!String.fromCharCode(charCode).match(/^[a-zA-Z0-9]*$/g))
+                return false;
+    });
+
+    $('.onlyNum').keypress(function (e) {
+        var charCode = (e.which) ? e.which : event.keyCode
+        if (String.fromCharCode(charCode).match(/[^0-9]/g))
+            return false;
+
+    });
+
+    $('.numWPts').keypress(function (e) {
+        var charCode = (e.which) ? e.which : event.keyCode
+        if (String.fromCharCode(charCode).match(/[^0-9\.]/g))
+            return false;
+
+    });
+
+    $('.onlyAlpha').keypress(function (e) {
+        var charCode = (e.which) ? e.which : event.keyCode
+        if (!String.fromCharCode(charCode).match(/^[a-zA-Z\s]*$/g))
+            return false;
+    });
+});
